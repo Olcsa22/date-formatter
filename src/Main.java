c3131f0561781c11ddd55bff30f068c9f8673f83
Bokros Olivér <bokrosoliver02@gmail.com>
2025-06-17 23:11:11 +0200

    feature(project): Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import enums.TimeConstants;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to my input reader!");
+        for(TimeConstants timeConstants : TimeConstants.values()){
+            System.out.println(timeConstants.unitName + " - " + timeConstants.inSeconds);
+        }
     }
 }
